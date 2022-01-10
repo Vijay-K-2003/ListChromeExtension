46a572219cb7f6dad451e5e257ab5b13b9afb918
Aryan Gupta <202051038@iiitvadodara.ac.in>
2022-01-11 00:16:45 +0530

    when we added data in the list then form was reloading that's why our list was disappearing so, i did some little changes for prevention of disappearing the list

--- popup.js
+++ popup.js
@@ -1,31 +1,15 @@
-document.getElementById("btn").addEventListener("click", addVal);
-
-let arr = [];
-let values;
-function getData() {
-    chrome.storage.local.get("valuesZ", function(result){
-        if(result.key != undefined){
-            values = result;
-        }
-    });    
-}
-getData();
-
-const list = document.getElementById('list-content');
-function addVal() {
-    const listItem = document.getElementById(
-        'list-item-new'
-    );
-    const val = listItem.value;
-    arr.push(val);
-    console.log(arr);
-    values = "";
-    
-    for(var i = 0; i < arr.length; i++) {
-        values += "<li>" + arr[i] + "</li>";
+let arr=[];
+const input=document.getElementById("list-item-new");
+const action=document.getElementById("btn");
+const container=document.getElementById("list-content");
+action.addEventListener('click',()=>{
+    let values=input.value;
+    let str="";
+    const a=`<li>${values}</li>`;
+    container.insertAdjacentHTML("beforeend",a);
+    arr.push(values);
+    for(var i=0;i<arr.length;i++){
+           str+=`<li>${arr[i]}</li>`;
     }
-
-    list.innerHTML = values;
-    chrome.storage.local.set({"valuesZ": values});
-    getData();
-}
+    input.value="";
+})
